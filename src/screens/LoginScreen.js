69f81fd88a79c83ce3834b6015fba6d5c5d49599
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-13 13:29:05 +0200

    Coding.

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,19 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
-import Button from "../components/Button";
-export function LoginScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text>Login Screen</Text>
-      <Button label="Goto home" onPress={() => navigation.navigate("Home")} />
-    </View>
-  );
-}
+import React from "react";
+import {
+  StyleContainer,
+  InnerContainer,
+  PageTitle,
+} from "../components/styled";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+const LoginScreen = () => {
+  <StyleContainer>
+    <InnerContainer>
+      <PageTitle>Login</PageTitle>
+    </InnerContainer>
+  </StyleContainer>;
+};
+
+export default LoginScreen;
