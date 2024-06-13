10c3214716b7f88259fab67dc814552e8bdcadcb
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-13 19:56:17 +0200

    Coding login screen.

--- src/components/styled.js
+++ src/components/styled.js
@@ -1,5 +1,5 @@
-import styled from "styled-components";
-import { View, Text } from "react-native";
+import styled from "styled-components/native";
+import { View, Text, TextInput, TouchableOpacity } from "react-native";
 import Constants from "expo-constants";
 
 const StatusBarHeight = Constants.statusBarHeight;
@@ -31,10 +31,61 @@ export const InnerContainer = styled.View`
   align-items: center;
 `;
 
-export const PageTile = styled.Text`
+export const PageTitle = styled.Text`
   font-size: 30px;
   text-align: center;
   font-weight: bold;
   color: ${brand};
   padding: 10px;
+`;
+
+export const StyledFormArea = styled.View`
+  width: 90%;
+`;
+
+export const StyledTextInput = styled.TextInput`
+  background-color: ${secondary};
+  padding: 15px;
+  padding-left: 55px;
+  padding-right: 55px;
+  border-radius: 5px;
+  font-size: 16px;
+  height: 60px;
+  margin-vertical: 3px;
+  margin-bottom: 10px;
+  color: ${tertiary};
+`;
+
+export const StyledInputLabel = styled.Text`
+  color: ${tertiary};
+  font-size: 13px;
+  text-align: left;
+`;
+
+export const StyledButton = styled.TouchableOpacity`
+  padding: 15px;
+  background-color: ${brand};
+  justify-content: center;
+  border-radius: 5px;
+  margin-vertical: 5px;
+  height: 60px;
+`;
+
+export const StyledButtonText = styled.Text`
+  color: ${primary};
+  font-size: 16px;
+`;
+
+export const LeftIcon = styled.View`
+  left: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
+`;
+
+export const RightIcon = styled.TouchableOpacity`
+  right: 15px;
+  top: 38px;
+  position: absolute;
+  z-index: 1;
 `;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,16 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
+import { View } from "react-native";
 import {
-  StyleContainer,
+  StyledContainer,
   InnerContainer,
-  PageTitle,
+  StyledFormArea,
+  StyledInputLabel,
+  StyledTextInput,
+  Colors,
+  LeftIcon,
+  RightIcon,
 } from "../components/styled";
 
-const LoginScreen = () => {
-  <StyleContainer>
-    <InnerContainer>
-      <PageTitle>Login</PageTitle>
-    </InnerContainer>
-  </StyleContainer>;
-};
+import { Octicons, Ionicons } from "@expo/vector-icons";
 
-export default LoginScreen;
+const { darkLight, brand } = Colors;
+
+import { Formik } from "formik";
+
+export function LoginScreen({ navigation }) {
+  const [hidePassword, setHidePassword] = useState(true);
+  return (
+    <StyledContainer>
+      <InnerContainer>
+        <Formik
+          initialValues={{ user: "", password: "" }}
+          onSubmit={(values) => console.log(values)}
+        >
+          {({ handleChange, handleBlur, handleSubmit, values }) => (
+            <StyledFormArea>
+              <MyTextInput
+                icon="mail"
+                label="Username"
+                placeholder="andy"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("user")}
+                onBlur={handleBlur("user")}
+                value={values.user}
+              />
+              <MyTextInput
+                icon="lock"
+                label="Password"
+                placeholder="* * * * * *"
+                placeholderTextColor={darkLight}
+                onChangeText={handleChange("password")}
+                onBlur={handleBlur("password")}
+                value={values.password}
+                secureTextEntry={hidePassword}
+                isPassword={true}
+                hidePassword={hidePassword}
+                setHidePassword={setHidePassword}
+              />
+            </StyledFormArea>
+          )}
+        </Formik>
+      </InnerContainer>
+    </StyledContainer>
+  );
+}
+
+const MyTextInput = ({
+  icon,
+  label,
+  isPassword,
+  hidePassword,
+  setHidePassword,
+  ...props
+}) => {
+  return (
+    <View>
+      <LeftIcon>
+        <Octicons name={icon} size={30} color={brand} />
+      </LeftIcon>
+      <StyledInputLabel>{label}</StyledInputLabel>
+      <StyledTextInput {...props} />
+      {isPassword && (
+        <RightIcon onPress={() => setHidePassword(!hidePassword)}>
+          <Ionicons
+            name={hidePassword ? "eye-off" : "eye"}
+            size={30}
+            color={darkLight}
+          />
+        </RightIcon>
+      )}
+    </View>
+  );
+};
