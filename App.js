c820d47a80e2a67d08b0b2b1b136c7f958682c7e
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-13 19:55:57 +0200

    Removed not needed imports.

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "./src/screens/HomeScreen";
@@ -19,12 +17,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#25292e",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
