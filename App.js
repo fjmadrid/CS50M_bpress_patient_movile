bdd69db352fb8e016b3216ba8994c8fae9f77b25
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-14 17:13:24 +0200

    Set styles to navigation bar.

--- App.js
+++ App.js
@@ -4,13 +4,23 @@ import { WelcomeScreen } from "./src/screens/WelcomeScreen";
 import { HomeScreen } from "./src/screens/HomeScreen";
 import { LoginScreen } from "./src/screens/LoginScreen";
 import { DetailsScreen } from "./src/screens/DetailsScreen";
-
+import { Colors } from "./src/components/styled";
+const { brand, primary } = Colors;
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: brand,
+          },
+          headerTintColor: primary,
+          headerTitleAlign: "center",
+        }}
+        initialRouteName="Welcome"
+      >
         <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Home" component={HomeScreen} />
