791bf5ffbe172a13b0491d5585d7a9a0addba388
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-17 13:20:25 +0200

    Added

---
+++ src/api/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+import { API_URL } from "../constants";
+
+export const login = async (credentials, setPatient, setDoctor) => {
+  const resp1 = await axios.post(
+    API_URL + "authentication/login/",
+    credentials
+  );
+
+  if (resp1.status === 200) {
+    const key = resp1.data["key"];
+    axios.defaults.headers.common["Authorization"] = `Token ${key}`;
+
+    const resp2 = await axios.get(API_URL + "patient/", credentials);
+
+    if (resp2.status === 200) {
+      setPatient(resp2.data["user"]);
+
+      const resp3 = await axios.get(API_URL + "patient/doctor");
+
+      if (resp3.status === 200) {
+        setDoctor(resp3.data);
+      } else {
+        console.log(`Error ${resp3.status} getting doctor: ${resp3.error}`);
+      }
+    } else {
+      console.log(`Error ${resp2.status} getting patient: ${resp2.error}`);
+    }
+  } else {
+    console.log(`Error ${resp1.status} getting token: ${resp1.error}`);
+  }
+};
