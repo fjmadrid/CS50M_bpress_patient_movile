725c7dd5add5d7802ae3e63a011fdeff369acae3
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-18 14:18:00 +0200

    Add a splash screen.

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import { LoginScreen } from "./src/screens/LoginScreen";
 import { DetailsScreen } from "./src/screens/DetailsScreen";
 import { Colors } from "./src/components/styled";
 import { SignupScreen } from "./src/screens/SignupScreen";
+import { SplashScreen } from "./src/screens/SplashScreen";
 import { Provider } from "react-redux";
 import store from "./src/state/store";
 
@@ -24,8 +25,9 @@ export default function App() {
             headerTintColor: primary,
             headerTitleAlign: "center",
           }}
-          initialRouteName="Login"
+          initialRouteName="Splash"
         >
+          <Stack.Screen name="Splash" component={SplashScreen} />
           <Stack.Screen name="Welcome" component={WelcomeScreen} />
           <Stack.Screen name="Login" component={LoginScreen} />
           <Stack.Screen name="Signup" component={SignupScreen} />
---
+++ src/screens/SplashScreen.js
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import { ActivityIndicator } from "react-native-web";
+import { StatusBar } from "expo-status-bar";
+import { InnerContainer } from "../components/styled";
+import { useDispatch, useSelector } from "react-redux";
+import { selectSessionToken } from "../state/sessionSlice";
+import { fetchPatient, selectPatientFetchStatus } from "../state/patientSlice";
+import { fetchDoctor, selectDoctorFetchStatus } from "../state/doctorSlice";
+import axios from "axios";
+
+export function SplashScreen({ navigation }) {
+  const token = useSelector(selectSessionToken);
+  const patientFetchStatus = useSelector(selectPatientFetchStatus);
+  const doctorFetchStatus = useSelector(selectDoctorFetchStatus);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (token === "") {
+      console.log(`Not valid token: "${token}"`);
+      console.log("Navigate to Login screen");
+      navigation.navigate("Login");
+    } else if (patientFetchStatus === "idle" || doctorFetchStatus === "idle") {
+      console.log(`Valid token: "${token}"`);
+      axios.defaults.headers.common["Authorization"] = `Token ${token}`;
+      if (patientFetchStatus === "idle") dispatch(fetchPatient());
+      //if (doctorFetchStatus === "idle") dispatch(fetchDoctor());
+    } else if (
+      patientFetchStatus === "succeeded" &&
+      doctorFetchStatus === "succeeded"
+    ) {
+      navigation.navigate("Welcome");
+    } else if (
+      patientFetchStatus === "failed" ||
+      doctorFetchStatus === "failed"
+    ) {
+      navigation.navigate("Login");
+    }
+  }, [dispatch, navigation, token, patientFetchStatus, doctorFetchStatus]);
+  console.log("En splash screen!");
+  return (
+    <>
+      <StatusBar style="light" />
+      <InnerContainer>
+        {(patientFetchStatus === "loading" ||
+          doctorFetchStatus === "loading") && (
+          <ActivityIndicator size="large" />
+        )}
+      </InnerContainer>
+    </>
+  );
+}
