958970fe66ba59c154eec522f766af13a8ad4bde
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-20 17:49:56 +0200

    Fixed bug in router. Recode to use Redirect to bifurcate between Login/Welcome screens.

--- app/index.js
+++ app/index.js
@@ -1,38 +1,19 @@
-import { useEffect } from "react";
-import { StatusBar } from "expo-status-bar";
-import { InnerContainer } from "../src/components/styled";
 import { useSelector } from "react-redux";
 import { selectSessionToken } from "../src/state/sessionSlice";
 import axios from "axios";
-import { router, Slot, useRootNavigationState } from "expo-router";
+import { Redirect } from "expo-router";
 
 export default function SplashScreen() {
-  const rootNavigationState = useRootNavigationState();
   const token = useSelector(selectSessionToken);
-
-  useEffect(() => {
-    if (token === "") {
-      console.log(`Not valid token: "${token}"`);
-      console.log("Navigate to Login screen");
-      router.navigate("/login");
-    } else {
-      console.log(`Valid token: "${token}"`);
-      axios.defaults.headers.common["Authorization"] = `Token ${token}`;
-      router.navigate("/welcome");
-    }
-  }, [token]);
-
-  if (rootNavigationState?.key) {
-    console.log("In splash screen!. root navigator does not initialized yet.");
-    return null;
+  if (token !== "") {
+    axios.defaults.headers.common["Authorization"] = `Token ${token}`;
   }
-
   console.log(`In splash screen!. session token:"${token}"`);
 
   return (
-    <Slot>
-      <StatusBar style="light" />
-      <InnerContainer></InnerContainer>
-    </Slot>
+    <>
+      {token === "" && <Redirect href="/login" />}
+      {token !== "" && <Redirect href="/welcome" />}
+    </>
   );
 }
