7101544bd4dcd8da8a4a8ae4779c0369827dfbff
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-20 17:50:21 +0200

    Fixed bug in specified route.

--- app/login.js
+++ app/login.js
@@ -44,7 +44,7 @@ export default function LoginScreen() {
 
   useEffect(() => {
     if (loginStatus === "succeeded") {
-      router.navigate("Welcome");
+      router.navigate("/welcome");
     }
   }, [loginStatus]);
 
