fd66798bdcf6d1cab7fcbb6186d89a12c45479a1
Francisco José Madrid Cuevas <fjmadrid@uco.es>
2024-06-26 18:39:54 +0200

    Dispatch add new measurement action.

--- app/(home)/(measurements)/add/index.js
+++ app/(home)/(measurements)/add/index.js
@@ -1,5 +1,10 @@
-import React, { useState } from "react";
-import { View, TouchableOpacity, Pressable } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  TouchableOpacity,
+  Pressable,
+  ActivityIndicator,
+} from "react-native";
 import DateTimePicker from "@react-native-community/datetimepicker";
 
 import {
@@ -14,18 +19,35 @@ import {
   MsgBox,
 } from "../../../../src/components/styled";
 
+import {
+  addNewMeasurement,
+  selectMeasurementsError,
+  selectMeasurementsStatus,
+} from "../../../../src/state/measurementsSlice";
+
+import { selectPatientId } from "../../../../src/state/patientSlice";
+
 import { Formik } from "formik";
 
 import KeyboardAvoidingWrapper from "../../../../src/components/KeyboardAvoidingWrapper";
 
 import { router } from "expo-router";
+import { useDispatch, useSelector } from "react-redux";
 
 const { darkLight, brand } = Colors;
 
 export default function AddNewMeasurementScreen() {
+  const measurementsStatus = useSelector(selectMeasurementsStatus);
+  const measurementsError = useSelector(selectMeasurementsError);
+  const patientId = useSelector(selectPatientId());
+  const dispatch = useDispatch();
   const [showDatePicker, setShowDatePicker] = useState(false);
   const [showTimePicker, setShowTimePicker] = useState(false);
   const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    if (measurementsStatus === "succeeded") router.back();
+  }, [measurementsStatus]);
 
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate;
@@ -43,117 +65,124 @@ export default function AddNewMeasurementScreen() {
   };
 
   return (
-    <KeyboardAvoidingWrapper>
-      <StyledContainer>
-        <InnerContainer>
-          {showDatePicker && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode="date"
-              is24Hour={true}
-              display="default"
-              onChange={onChange}
-            />
-          )}
-          {showTimePicker && (
-            <DateTimePicker
-              testID="dateTimePicker"
-              value={date}
-              mode="time"
-              is24Hour={true}
-              display="default"
-              onChange={onChange}
-            />
-          )}
-          <Formik
-            initialValues={{
-              systolic: "",
-              diastolic: "",
-              ppm: "",
-              observation: "",
-              date: "",
-            }}
-            onSubmit={(values) => {
-              values.date = date.toISOString();
-              console.log(
-                `Added new measurement with values: ${JSON.stringify(values)}`
-              );
-              router.back();
-            }}
-          >
-            {({ handleChange, handleBlur, handleSubmit, values }) => (
-              <StyledFormArea>
-                <MyTextInput
-                  label="Systolic pressure"
-                  placeholder="120"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("systolic")}
-                  onBlur={handleBlur("systolic")}
-                  value={values.systolic}
-                  keyboardType="numeric"
+    <>
+      {measurementsStatus === "loading" && <ActivityIndicator size="large" />}
+      {measurementsStatus !== "loading" && (
+        <KeyboardAvoidingWrapper>
+          <StyledContainer>
+            <InnerContainer>
+              {showDatePicker && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode="date"
+                  is24Hour={true}
+                  display="default"
+                  onChange={onChange}
                 />
-                <MyTextInput
-                  label="Diastolic pressure"
-                  placeholder="70"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("diastolic")}
-                  onBlur={handleBlur("diastolic")}
-                  value={values.diastolic}
-                  keyboardType="numeric"
+              )}
+              {showTimePicker && (
+                <DateTimePicker
+                  testID="dateTimePicker"
+                  value={date}
+                  mode="time"
+                  is24Hour={true}
+                  display="default"
+                  onChange={onChange}
                 />
-                <MyTextInput
-                  label="Pulses per minute"
-                  placeholder="70"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("ppm")}
-                  onBlur={handleBlur("ppm")}
-                  value={values.ppm}
-                  keyboardType="numeric"
-                />
-                <MyTextInput
-                  label="Date"
-                  placeholder="YYYY - MM - DD"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("date")}
-                  onBlur={handleBlur("date")}
-                  value={date.toDateString()}
-                  isDateTime={true}
-                  editable={false}
-                  activateDateTimePicker={activateDatePicker}
-                />
-                <MyTextInput
-                  label="Time"
-                  placeholder="HH : MM"
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("date")}
-                  onBlur={handleBlur("date")}
-                  value={date.toDateString()}
-                  isTimeTime={true}
-                  editable={false}
-                  activateDateTimePicker={activateTimePicker}
-                />
-                <MyTextInput
-                  label="Observation"
-                  placeholder="Write down any observation here."
-                  placeholderTextColor={darkLight}
-                  onChangeText={handleChange("observation")}
-                  onBlur={handleBlur("observation")}
-                  value={values.observation}
-                  multiline={true}
-                  numberOfLines={5}
-                  activateDateTimePicker={activateTimePicker}
-                />
-                <MsgBox>...</MsgBox>
-                <StyledButton onPress={handleSubmit}>
-                  <StyledButtonText>Add new measurement</StyledButtonText>
-                </StyledButton>
-              </StyledFormArea>
-            )}
-          </Formik>
-        </InnerContainer>
-      </StyledContainer>
-    </KeyboardAvoidingWrapper>
+              )}
+              <Formik
+                initialValues={{
+                  systolic: "",
+                  diastolic: "",
+                  ppm: "",
+                  observation: "",
+                  date: "",
+                }}
+                onSubmit={(values) => {
+                  values.date = date.toISOString();
+                  console.log(
+                    `Added new measurement with values: ${JSON.stringify(
+                      values
+                    )}`
+                  );
+                  dispatch(addNewMeasurement({ id: patientId, data: values }));
+                }}
+              >
+                {({ handleChange, handleBlur, handleSubmit, values }) => (
+                  <StyledFormArea>
+                    <MyTextInput
+                      label="Systolic pressure"
+                      placeholder="120"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("systolic")}
+                      onBlur={handleBlur("systolic")}
+                      value={values.systolic}
+                      keyboardType="numeric"
+                    />
+                    <MyTextInput
+                      label="Diastolic pressure"
+                      placeholder="70"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("diastolic")}
+                      onBlur={handleBlur("diastolic")}
+                      value={values.diastolic}
+                      keyboardType="numeric"
+                    />
+                    <MyTextInput
+                      label="Pulses per minute"
+                      placeholder="70"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("ppm")}
+                      onBlur={handleBlur("ppm")}
+                      value={values.ppm}
+                      keyboardType="numeric"
+                    />
+                    <MyTextInput
+                      label="Date"
+                      placeholder="YYYY - MM - DD"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("date")}
+                      onBlur={handleBlur("date")}
+                      value={date.toDateString()}
+                      isDateTime={true}
+                      editable={false}
+                      activateDateTimePicker={activateDatePicker}
+                    />
+                    <MyTextInput
+                      label="Time"
+                      placeholder="HH : MM"
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("date")}
+                      onBlur={handleBlur("date")}
+                      value={date.toDateString()}
+                      isTimeTime={true}
+                      editable={false}
+                      activateDateTimePicker={activateTimePicker}
+                    />
+                    <MyTextInput
+                      label="Observation"
+                      placeholder="Write down any observation here."
+                      placeholderTextColor={darkLight}
+                      onChangeText={handleChange("observation")}
+                      onBlur={handleBlur("observation")}
+                      value={values.observation}
+                      multiline={true}
+                      numberOfLines={5}
+                      activateDateTimePicker={activateTimePicker}
+                    />
+                    <MsgBox>{measurementsError}</MsgBox>
+                    <StyledButton onPress={handleSubmit}>
+                      <StyledButtonText>Add new measurement</StyledButtonText>
+                    </StyledButton>
+                  </StyledFormArea>
+                )}
+              </Formik>
+            </InnerContainer>
+          </StyledContainer>
+        </KeyboardAvoidingWrapper>
+      )}
+    </>
   );
 }
 
